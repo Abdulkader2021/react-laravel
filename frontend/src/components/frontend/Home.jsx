2f794fd4a96165c7ad611fd08277672f1d547389
Abdulkader2021 <abdulkader28@outlook.com>
2026-04-03 06:58:49 +0600

    Service, Why choose us, card

--- frontend/src/components/frontend/Home.jsx
+++ frontend/src/components/frontend/Home.jsx
@@ -5,9 +5,9 @@ import BuildingImg from '../../assets/images/construction1.jpg';
 import ResidentialImg from '../../assets/images/construction2.jpg';
 import CorporateImg from '../../assets/images/construction3.jpg';
 
-import Project1 from '../../assets/images/icon-1.svg';
-import Project2 from '../../assets/images/icon-2.svg';
-import Project3 from '../../assets/images/icon-3.svg';
+import Icon1 from '../../assets/images/icon-1.svg';
+import Icon2 from '../../assets/images/icon-2.svg';
+import Icon3 from '../../assets/images/icon-3.svg';
 
 import Header from '../common/Header';
 import Footer from '../common/footer';
@@ -68,42 +68,140 @@ const Home = () => {
             </section>
 
             {/* Our Section */}
-
-           <section className="section-3">
-            <div className="container">
-                <div className="section-header">
+           <section className="section-3 bg-light py-5">
+            <div className="container-flud py-5">
+                <div className="section-header text-center">
                     <span>Our Services</span>
                     <h2>Our construction services</h2>
                     <p>We offer a diverse array of construction services, spanning residential, commercial, and industrial projects.</p>
                 </div>
 
-                <div className="row">
+                <div className="row pt-4">
+
                     {/* Card 1 */}
-                    <div className="col-md-4">
-                        <div className="card service-card" style={{ backgroundImage: `url(${BuildingImg})` }}>
-                            <div className="card-body">
-                                <h4>Building Construction</h4>
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={BuildingImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Residential Construction</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
                             </div>
                         </div>
                     </div>
 
-                    {/* Card 2 - Active/Hover state style */}
-                    <div className="col-md-4">
-                        <div className="card service-card active" style={{ backgroundImage: `url(${ResidentialImg})` }}>
-                            <div className="card-body">
-                                <h4>Residential Construction</h4>
-                                <p>Residential construction is a fundamental sector within the construction industry, dedicated to creating living spaces that meet the diverse needs of individuals and families.</p>
-                                <a href="#" className="btn btn-primary">Read More</a>
+                    {/* Card 2 */}
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={BuildingImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Residential Construction</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
                             </div>
                         </div>
                     </div>
 
                     {/* Card 3 */}
-                    <div className="col-md-4">
-                        <div className="card service-card" style={{ backgroundImage: `url(${CorporateImg})` }}>
-                            <div className="card-body">
-                                <h4>Corporate Construction</h4>
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={BuildingImg} alt="" className='w-100' />
                             </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Residential Construction</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Card 4 */}
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={BuildingImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Residential Construction</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
+                            </div>
+                        </div>
+                    </div>                   
+                </div>
+            </div>
+        </section>
+
+
+
+ {/* Why choose us */}
+        <section className='section-4 py-5'>
+            <div className="container py-5">
+                <div className="section-header text-center">
+                    <span>Why Choose Us</span>
+                    <h2>Discover our wide variety of projects</h2>
+                    <p>We offer a diverse array of construction services, spanning residential, commercial, and industrial projects.</p>
+                </div>
+
+                <div className="row pt-4">
+                    <div className="col-md-4">
+                        <div className="card shadow border-0 p-4">
+                            <div className="card-icon">
+                                <img src={Icon1} alt="" />
+                            </div>
+                            <div className="card-title mt-3">
+                                <h3>Cutting-Edge Solutions</h3>
+                            </div>
+                            <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laborum delectus omnis iste accusantium aliquam, vel sequi, eos amet ullam atque enim, quos deserunt aspernatur quidem quae sapiente! Nesciunt, consectetur?
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card shadow border-0 p-4">
+                            <div className="card-icon">
+                                <img src={Icon2} alt="" />
+                            </div>
+                            <div className="card-title mt-3">
+                                <h3>Cutting-Edge Solutions</h3>
+                            </div>
+                            <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laborum delectus omnis iste accusantium aliquam, vel sequi, eos amet ullam atque enim, quos deserunt aspernatur quidem quae sapiente! Nesciunt, consectetur?
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card shadow border-0 p-4">
+                            <div className="card-icon">
+                                <img src={Icon3} alt="" />
+                            </div>
+                            <div className="card-title mt-3">
+                                <h3>Cutting-Edge Solutions</h3>
+                            </div>
+                            <p>
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. In laborum delectus omnis iste accusantium aliquam, vel sequi, eos amet ullam atque enim, quos deserunt aspernatur quidem quae sapiente! Nesciunt, consectetur?
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -111,52 +209,99 @@ const Home = () => {
         </section>
 
 
-        {/* Our Projects Section */}
-        {/* Our Services Section */}
-<section className="section-4 bg-light py-5">
-    <div className="container py-5">
-        <div className='section-header text-center mb-5'>
-            <span>Our Services</span>
-            <h2>Excellence in every build</h2>
-            <p>We offer a wide range of construction services tailored to meet your specific needs.</p>
-        </div>
 
-        <div className="row">
-            {/* Service 1 */}
-            <div className="col-md-4 mb-4">
-                <div className="card border-0 shadow-sm p-4 text-center h-100">
-                    <div className="service-icon mb-3">
-                        <i className="bi bi-house-door fs-1 text-primary"></i>
+
+        {/* Our Projects */}
+        <section className="section-3 bg-light py-5">
+            <div className="container-flud py-5">
+                <div className="section-header text-center">
+                    <span>Our Projects</span>
+                    <h2>Discover our diverse range of projects</h2>
+                    <p>We offer a diverse array of construction services, spanning residential, commercial, and industrial projects.</p>
+                </div>
+
+                <div className="row pt-4">
+
+                    {/* Card 1 */}
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={ResidentialImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Dhaka Projects</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
+                            </div>
+                        </div>
                     </div>
-                    <h4>Residential Construction</h4>
-                    <p>Building beautiful, sustainable homes designed for modern living and comfort.</p>
+
+                    {/* Card 2 */}
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={ResidentialImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Dhaka Projects</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Card 3 */}
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={ResidentialImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Dhaka Projects</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Card 4 */}
+                    <div className="col-md-3 col-lg-3">
+                        <div className="item">
+                            <div className="service-image">
+                                <img src={ResidentialImg} alt="" className='w-100' />
+                            </div>
+                            <div className="service-body">
+                                <div className="service-title">
+                                    <h3>Dhaka Projects</h3>                                    
+                                </div>
+                                <div className="service-content">
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quos vero, eveniet rem dolores ducimus quas deleniti reiciendis dolorum quisquam dolore rerum sunt repellendus iure ipsam aperiam provident omnis recusandae?</p>
+                                </div>
+                                <a href="#" className='btn btn-primary'>Read More</a>
+                            </div>
+                        </div>
+                    </div>                   
                 </div>
             </div>
+        </section>
 
-            {/* Service 2 */}
-            <div className="col-md-4 mb-4">
-                <div className="card border-0 shadow-sm p-4 text-center h-100">
-                    <div className="service-icon mb-3">
-                        <i className="bi bi-building fs-1 text-primary"></i>
-                    </div>
-                    <h4>Commercial Projects</h4>
-                    <p>Professional workspaces and retail environments built with efficiency and style.</p>
-                </div>
-            </div>
 
-            {/* Service 3 */}
-            <div className="col-md-4 mb-4">
-                <div className="card border-0 shadow-sm p-4 text-center h-100">
-                    <div className="service-icon mb-3">
-                        <i className="bi bi-tools fs-1 text-primary"></i>
-                    </div>
-                    <h4>Renovations</h4>
-                    <p>Revitalizing existing structures with expert craftsmanship and modern updates.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+       
+
+
+
 
         </main>
 
